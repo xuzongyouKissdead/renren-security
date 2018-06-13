bfcdd02c86190e56f9e587494e7d4848719c24fe
‘xuzongyouKissdead <xuzongyou@aliyun.com>
2018-06-13 20:55:45 +0800

    添加注解@Data 配置pom文件等

--- renren-api/src/main/java/io/renren/form/RegisterForm.java
+++ renren-api/src/main/java/io/renren/form/RegisterForm.java
@@ -18,6 +18,7 @@ package io.renren.form;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
@@ -29,28 +30,14 @@ import javax.validation.constraints.NotBlank;
  * @since 3.1.0 2018-01-25
  */
 @ApiModel(value = "注册表单")
+@Data
 public class RegisterForm {
     @ApiModelProperty(value = "手机号")
-    @NotBlank(message="手机号不能为空")
+    @NotBlank(message = "手机号不能为空")
     private String mobile;
 
     @ApiModelProperty(value = "密码")
-    @NotBlank(message="密码不能为空")
+    @NotBlank(message = "密码不能为空")
     private String password;
 
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
